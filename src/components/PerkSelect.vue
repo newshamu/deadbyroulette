4c2d28930943cfda84c1ac324a95696d2d764af0
newshamu <johnsonc.2423@gmail.com>
2019-12-27 22:38:28 -0500

    Reworked perk selection into buttons

--- src/components/PerkSelect.vue
+++ src/components/PerkSelect.vue
@@ -1,12 +1,18 @@
 <template>
   <v-container fluid>
-    <v-row class="d-flex justify-space-around" v-for="perk in perks" :key="perk.name">
-      <v-flex>
-        <v-checkbox
-          v-model="perk.active"
-          :label="perk.name"
-        />
-      </v-flex>
+    <v-row class="d-flex justify-space-around">
+      <v-col class="ma-2" cols="2" v-for="perk in perks" :key="perk.name">
+        <v-card tile hover>
+          <v-btn
+            v-model="perk.active"
+            :color="perk.active ? 'primary' : accent"
+            block
+            v-on:click="toggle(perk)"
+          >
+            {{ perk.name }}
+          </v-btn>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -19,6 +25,12 @@ export default {
   data() {
     return {
       sPerks: this.perks
+    }
+  },
+
+  methods: {
+    toggle(perk) {
+      perk.active = !perk.active;
     }
   }
 
